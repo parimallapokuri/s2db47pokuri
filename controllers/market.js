0c51c1d6c16a2356d8f1cf4ba9dc5ad28b68d030
parimallapokuri <pokuriparimalla2000@gmail.com>
2022-04-13 21:28:06 -0500

    ADD

--- controllers/market.js
+++ controllers/market.js
@@ -11,8 +11,23 @@ exports.market_detail = function(req, res) {
 }; 
  
 // Handle market create on POST. 
-exports.market_create_post = function(req, res) { 
-    res.send('NOT IMPLEMENTED: market create POST'); 
+exports.market_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new market(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    document.itemType = req.body.itemType; 
+    document.Quantity = req.body.Quantity; 
+    document.price = req.body.price; 
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
  
 // Handle market delete form on DELETE. 
@@ -25,14 +40,27 @@ exports.market_update_put = function(req, res) {
     res.send('NOT IMPLEMENTED: market update PUT' + req.params.id); 
 }; 
 
-// List of all market 
-exports.marketlist = async function(req, res) { 
-    try{ 
-        themarket = await marketfind(); 
-        res.send(themarket); 
-    } 
-    catch(err){ 
-        res.status(500); 
-        res.send(`{"error": ${err}}`); 
-    }   
-}; 
+// List of all markets
+exports.market_list = async function(req, res) {
+    try{
+    themarket = await market.find();
+    res.send(themarket);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+};
+
+// VIEWS
+// Handle a show all view
+exports.market_view_all_Page = async function(req, res) {
+    try{
+    themarket = await market.find();
+    res.render('market', { title: 'market Search Results', results: themarket });
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+};
--- routes/market.js
+++ routes/market.js
@@ -1,9 +1,12 @@
 var express = require('express');
+const market_controlers= require('../controllers/market'); 
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('market', { title: 'Search Results Market' });
-});
+//router.get('/', function(req, res, next) {
+ // res.render('market', { title: 'Search Results Market' });
+//});
+
+router.get('/', market_controlers.market_view_all_Page ); 
 
 module.exports = router;
