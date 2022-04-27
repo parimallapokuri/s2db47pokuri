b7bb9550ece64409e21d7c01b8b043e1db309f40
parimallapokuri <pokuriparimalla2000@gmail.com>
2022-04-27 12:17:37 -0500

    upd

--- routes/market.js
+++ routes/market.js
@@ -1,25 +1,36 @@
 var express = require('express');
-const market_controlers= require('../controllers/market'); 
+const market_controller = require('../controllers/market');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on 
+//or
+// redirect to login. 
+const secured = (req, res, next) => {
+  if (req.user) {
+    return next();
+  }
+  req.session.returnTo = req.originalUrl;
+  res.redirect("/login");
+}
 
 /* GET home page. */
-//router.get('/', function(req, res, next) {
- // res.render('market', { title: 'Search Results Market' });
-//});
+/*
+router.get('/', function(req, res, next) {
+  res.render('markets', { title: 'Search Results' });
+});
+*/
+/* GET detail costume page */
+router.get('/detail', market_controller.market_view_one_Page);
 
-//router.get('/', market_controlers.market_view_all_Page ); 
+/* GET create costume page */
+router.get('/create',secured, market_controller.market_create_Page);
 
-// GET request for one market. 
-//router.get('/market/:id', market_controlers.market_detail); 
+/* GET costumes */
+router.get('/', market_controller.market_view_all_Page);
 
-/* GET detail market page */ 
-router.get('/detail', market_controlers.market_view_one_Page); 
-/* GET create market page */ 
-router.get('/create', market_controlers.market_create_Page); 
-/* GET create update page */ 
-router.get('/update', market_controlers.market_update_Page); 
-/* GET delete market page */ 
-router.get('/delete', market_controlers.market_delete_Page); 
- 
+/* GET create update page */
+router.get('/update',secured, market_controller.market_update_Page);
+
+/* GET delete costume page */
+router.get('/delete',secured,market_controller.market_delete_Page);
 
 module.exports = router;
